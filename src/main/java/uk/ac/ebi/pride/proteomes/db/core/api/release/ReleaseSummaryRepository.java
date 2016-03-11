38da415f405de8c5eca539648d01715a1debedc4
noedelta <noedelta@gmail.com>
2016-03-11 11:29:19 +0000

    Adds the Repository annotation that was forgotten.

--- src/main/java/uk/ac/ebi/pride/proteomes/db/core/api/release/ReleaseSummaryRepository.java
+++ src/main/java/uk/ac/ebi/pride/proteomes/db/core/api/release/ReleaseSummaryRepository.java
@@ -1,8 +1,16 @@
 package uk.ac.ebi.pride.proteomes.db.core.api.release;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.pride.proteomes.db.core.api.protein.Protein;
+
 /**
  * @author ntoro
  * @since 07/03/2016 16:54
  */
-public interface ReleaseSummaryRepository {
+@Repository
+@Transactional(readOnly = true)
+public interface ReleaseSummaryRepository extends JpaRepository<Protein, String>, QueryDslPredicateExecutor<Protein> {
 }
